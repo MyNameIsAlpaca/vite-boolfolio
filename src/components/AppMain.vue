337f63052a3470e11e4d64e666aa7e88c77e4d52
MyNameIsAlpaca <Saragosagabriele@gmail.com>
2023-05-29 16:26:07 +0200

    feat: milestone 5

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,26 +1,52 @@
 <script>
 import axios from 'axios';
+import AppProjectCard from './AppProjectCard.vue';
 
 export default{
-  name: 'AppMain',
-
-  data() {
-    return {
-      projects: [],
+    name: "AppMain",
+    data() {
+        return {
+            projects: [],
+        };
+    },
+    created() {
+        this.getProjects();
+    },
+    methods: {
+        getProjects() {
+            axios.get("http://127.0.0.1:8000/api/projects").then(response => {
+              this.projects = response.data.results
+            });
+        }
+    },
+    components: { 
+      AppProjectCard,
     }
-  },
-
-  created(){
-    this.getProjects();
-  },
-
-  methods: {
-    getProjects(){
-      axios.get('http://127.0.0.1:8000/api/projects').then(response => {
-        console.log(response);
-      })
-    }
-  }
 }
 
 </script>
+
+<template>
+  <div class="container">
+    <h1>I miei progetti</h1>
+    <div class="card-container">
+      <div v-for="project in projects" class="bt-2">
+        <AppProjectCard :project="project"></AppProjectCard>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .container{
+    h1{
+      text-align: center;
+      padding-top: 20px;
+    }
+    .card-container{
+      display: flex;
+      gap: 20px;
+      padding-top: 30px;
+    }
+  }
+</style>
---
+++ src/components/AppProjectCard.vue
@@ -0,0 +1,39 @@
+<script>
+
+export default{
+  name: 'AppProjectCard',
+
+  data() {
+    return {
+      baseUrl:'http://127.0.0.1:8000/',
+    }
+  },
+
+  props: {
+
+    project: Object,
+
+  },
+
+  created(){
+  },
+
+  methods: {
+  }
+}
+
+</script>
+
+<template>
+  <div class="card" style="width: 18rem;">
+    <!-- <img class="card-img-top" src="..." alt="Card image cap"> -->
+    <div class="card-body">
+      <h5 class="card-title">{{ project.name }}</h5>
+      <p class="card-text">{{ project.desc }}</p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+  
+</style>
