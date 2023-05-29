ad0ec6b82575799439fc59a00aff5cb9618f1175
MyNameIsAlpaca <Saragosagabriele@gmail.com>
2023-05-29 15:31:29 +0200

    feat: milestone 4

---
+++ src/components/AppMain.vue
@@ -0,0 +1,26 @@
+<script>
+import axios from 'axios';
+
+export default{
+  name: 'AppMain',
+
+  data() {
+    return {
+      projects: [],
+    }
+  },
+
+  created(){
+    this.getProjects();
+  },
+
+  methods: {
+    getProjects(){
+      axios.get('http://127.0.0.1:8000/api/projects').then(response => {
+        console.log(response);
+      })
+    }
+  }
+}
+
+</script>
