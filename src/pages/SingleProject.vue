7be837e67455fc105daa1ab7e70702356c47a147
MyNameIsAlpaca <Saragosagabriele@gmail.com>
2023-05-31 17:31:45 +0200

    feat: axios call for single project

--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -1,20 +1,32 @@
 <script>
+import axios from 'axios';
+
 export default {
   name: 'SingleProject',
   data(){
     return{
+      project: {},
       projectSlug: '',
     }
   },
   mounted(){
     this.projectSlug = this.$route.params.slug;
+    this.getProject();
+  },
+
+  methods: {
+    getProject(){
+      axios.get('http://127.0.0.1:8000/api/projects/' + this.projectSlug).then(response => {
+        this.project = response.data.project
+      });
+    }
   }
 }
 </script>
 
 <template>
   <div>
-    <h1>Benvenuto nel progetto</h1>
+    <h1>{{ project.name }}</h1>
 
   </div>
 </template>
